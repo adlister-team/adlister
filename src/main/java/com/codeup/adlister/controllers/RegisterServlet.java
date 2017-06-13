d141930c38e3b399674ec8daabd31ad3a2f081d8
emilio alvarado <emiliolvrd@gmail.com>
2017-06-13 15:36:45 -0500

    finished step 2 of passwords exercise

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -23,6 +23,7 @@ public class RegisterServlet extends HttpServlet {
         String password = request.getParameter("password");
         String passwordConfirmation = request.getParameter("confirm_password");
 
+        String hash = Password.hash(password);
 
         // validate input
         boolean inputHasErrors = username.isEmpty()
@@ -36,8 +37,10 @@ public class RegisterServlet extends HttpServlet {
         }
 
         // create and save a new user
-        User user = new User(username, email, password);
+        User user = new User(username, email, hash);
         DaoFactory.getUsersDao().insert(user);
         response.sendRedirect("/login");
+
+        System.out.println(hash);
     }
 }
