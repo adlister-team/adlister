1f1a33c496b8711ee2f7fda5fb6a07c88f65fc05
emilio alvarado <emiliolvrd@gmail.com>
2017-06-14 10:24:18 -0500

    started on show ads info created ad info jsp created ad info servlet

---
+++ src/main/java/com/codeup/adlister/controllers/AdsInfoServlet.java
@@ -0,0 +1,24 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by emilioalvarado on 6/14/17.
+ */
+public class AdsInfoServlet {
+    @WebServlet(name = "controllers.AdsInfoServlet", urlPatterns = "/ads/info")
+    public class AdsIndexServlet extends HttpServlet {
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            request.setAttribute("ads", DaoFactory.getAdsDao());
+            request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+        }
+    }
+}
